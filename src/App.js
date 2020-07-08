66992840c6e3c80118282f5f7db1fa99a6f138bb
M-benjamin <mballabenjamin@yahoo.fr>
2020-07-08 16:14:55 +0200

    add router to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch } from "react-router-dom"
 
 import HomePage from "./pages/homepage/homepage.component"
 
@@ -7,9 +7,9 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <HomePage />
-      </div>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+      </Switch>
     )
   }
 }
