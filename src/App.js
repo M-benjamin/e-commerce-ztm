b94a96e06f9cdabdc72deeb5c99ee8cc707aa547
M-benjamin <mballabenjamin@yahoo.fr>
2020-07-14 20:38:25 +0200

    Adding collection item

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom"
 
 import HomePage from "./pages/homepage/homepage.component"
+import ShopPage from "./pages/shop/shop.component"
+
+import "./App.css"
 
 class App extends React.Component {
-
   render() {
     return (
       <Switch>
         <Route exact path="/" component={HomePage}/>
+        <Route path="/shop" component={ShopPage}/>
       </Switch>
     )
   }
